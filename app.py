73f4d677480907f5b80c7f16b72f9aea3f0ff567
byunboy2 <76236947+byunboy2@users.noreply.github.com>
2022-10-12 16:58:34 -0700

    Completed story generation

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
 
-from stories import silly_story
+from stories import silly_story, excited_story
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
@@ -12,4 +12,12 @@ debug = DebugToolbarExtension(app)
 def generate_form():
     """Generate and Display Form"""
 
-    return render_template("questions.html", words = silly_story.prompts)
+    return render_template("questions.html", words = excited_story.prompts)
+
+@app.get('/results')
+def generate_sentence():
+    """Generate and Display Story Based On What Was Passed In Query Param"""
+
+    input_words = request.args
+    return render_template("results.html",story=excited_story.generate(input_words))
+    # add new line to generate story 
